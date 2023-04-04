af2ec7b3586eebc5bdbdd4aa68cbf6844bbc3b81
Anton Tenev <anton.s.tenev@gmail.com>
2023-04-04 08:34:43 +0300

    connection error handling

--- app/Repositories/PostRepositoryEloquent.php
+++ app/Repositories/PostRepositoryEloquent.php
@@ -3,37 +3,57 @@
 namespace App\Repositories;
 
 use App\Models\Post;
-use Illuminate\Database\Eloquent\Collection;
 use App\Repositories\PostRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\QueryException;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Exception;
 
 class PostRepositoryEloquent implements PostRepositoryInterface
 {
     public function getAll(): LengthAwarePaginator
     {
-        return Post::withCount('likes')->orderByDesc('publish_date')->paginate(10);
-        
+        try {
+            return Post::withCount('likes')->orderByDesc('publish_date')->paginate(10);
+        } catch (QueryException $e) {
+            throw new Exception('Could not retrieve posts: ' . $e->getMessage());
+        }
     }
 
     public function getByField(string $name, string $operation, string $value): LengthAwarePaginator
     {
-        return Post::withCount('likes')->where($name, $operation, $value)->orderByDesc('publish_date')->paginate(10);
+        try {
+            return Post::withCount('likes')->where($name, $operation, $value)->orderByDesc('publish_date')->paginate(10);
+        } catch (QueryException $e) {
+            throw new Exception('Could not retrieve posts: ' . $e->getMessage());
+        }
     }
 
     public function create(array $data): Post
     {
-        return Post::create($data);
+        try {
+            return Post::create($data);
+        } catch (QueryException $e) {
+            throw new Exception('Could not create post: ' . $e->getMessage());
+        }
     }
 
     public function update(int $id, array $data): Post
     {
-        $post = Post::updateOrCreate(['id' => $id], $data);
-        return $post;
+        try {
+            $post = Post::updateOrCreate(['id' => $id], $data);
+            return $post;
+        } catch (QueryException $e) {
+            throw new Exception('Could not update post: ' . $e->getMessage());
+        }
     }
 
     public function delete(Post $post): void
     {
-        $post->delete();
+        try {
+            $post->delete();
+        } catch (QueryException $e) {
+            throw new Exception('Could not delete post: ' . $e->getMessage());
+        }
     }
 }
-
